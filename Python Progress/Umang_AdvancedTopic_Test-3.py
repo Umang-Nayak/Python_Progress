7727506784b6c43803298dc67ae496415c35ab60
Umang-Nayak <146411216+Umang-Nayak@users.noreply.github.com>
2023-10-26 11:29:57 +0530

    Umang_AdvancedTopic_Test-3.py

--- Python Progress/Umang_AdvancedTopic_Test-3.py
+++ Python Progress/Umang_AdvancedTopic_Test-3.py
@@ -1,6 +1,6 @@
 """
-Question :-
-    Write a Python program to compute the square of the first N Fibonacci numbers,
+1st Question :-
+    Write a Python program to compute the square of the first n Fibonacci numbers,
     using the map function and generate a list of the numbers. [UMANG]
 
 First 10 Fibonacci numbers:
@@ -10,35 +10,35 @@ After squaring said numbers of the list:
 [0, 1, 1, 4, 9, 25, 64, 169, 441, 1156]
 """
 
-try:
-    n = int(input("Enter Number = "))
-    f1 = 0
-    f2 = 1
-    fs = []
-    for i in range(n):
-        fs.append(f1)
-        f3 = f1 + f2
-        f1 = f2
-        f2 = f3
-    print(f"First {n} Fibonacci Numbers =", fs)  # Using f String
+try:    # Using Try Block To Catch Any Error
+    def fib(n):  # Using Function
+        f1 = 0
+        f2 = 1
+        fs = []
+        for i in range(n):
+            fs.append(f1)
+            f3 = f1 + f2
+            f1 = f2
+            f2 = f3
+        print(f"First {n} Fibonacci Numbers =", fs)  # Using f String
+        sq = lambda z: z * z  # Using Lambda Function
+        f_list = list(map(sq, fs))  # Using Map Function
+        print(f"First {n} Squared Fibonacci Numbers =", f_list)
 
-    sq = lambda z: z * z  # Using Lambda Function
+    num = int(input("Enter Number = "))
+    fib(num)    # Calling Function
 
-    f_list = list(map(sq, fs))  # Using Map Function
 
-    print(f"First {n} Squared Fibonacci Numbers =", f_list)
-
-except Exception as e:
+except Exception as e:  # Using Except Block To Handle Any Error
     print("\n!!! Some Error Occur !!!")
-    print(f"Error = {e}")
+    print(f"Error = {e}")   # Print Error type
 
-finally:
+finally:    # Finally block Always Execute
     print("\n<^-^> Thank You <^-^>")
 
 
-
 """
-Question :-
+2nd Question :-
     Write a Python program to create a decorator function of
     Fibonacci series up to n using Lambda. [UMANG]
 
@@ -50,34 +50,27 @@ Fibonacci series upto 6:
 [0, 1, 1, 2, 3, 5]
 """
 
-try:
-    def sample(fx):
-        def mfx(arg):
-            f1 = 0
-            f2 = 1
-            fl = []
-            for i in range(arg):
-                fl.append(f1)
-                f3 = f1 + f2
-                f1 = f2
-                f2 = f3
-            fx(arg)
-            print(fl)
-        return mfx
+try:    # Using Try Block To Catch Error
+    def fibonacci_decorator(func):  # Decorator Function
+        def wrapper(n):
+            fib = lambda x: x if x <= 1 else fib(x - 1) + fib(x - 2)  # Using Lambda Function
+            result = [fib(i) for i in range(n)]     # Using List Comprehension1
+            return func(result)
+        return wrapper
 
 
-    @sample
-    def add(x):
-        print(f"Fibonacci Series upto {x}:")
+    @fibonacci_decorator  # Using Decorator Function
+    def print_fibonacci_series(fib_series):
+        print(f"Fibonacci Series : {fib_series}")  # Using F-String
 
 
-    size = int(input("Enter Size = "))
-    add(size)
+    num = int(input("Enter Number = "))  # Change n to the desired number of Fibonacci numbers
+    print_fibonacci_series(num)  # Calling Function
 
 
-except Exception as e:
+except Exception as e:  # Using Except Block To Handle Error
     print("\n!!! SOME ERROR OCCUR !!!")
     print(f"Error = {e}")
 
-finally:
+finally:    # Finally Block Always Execute
     print("\n<^-^> Thank You <^-^>")
